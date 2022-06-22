7956aa2c58e4025d72235dbd160c0ec7b5be9f07
afinaputri <afina.putri@bukalapak.com>
2022-06-22 19:25:11 +0700

    add page object search result

--- src/test/java/PageObjects/SearchPage.java
+++ src/test/java/PageObjects/SearchPage.java
@@ -1,17 +1,17 @@
-package Pages;
-
-import Data.FormData;
+package PageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-import static java.lang.Thread.sleep;
+import java.util.List;
 
 public class SearchPage {
     WebDriver driver;
 
     By search_button = By.cssSelector(".site-header__item--desktop > .site-header__button");
     By search_field = By.id("search-input--desktop");
+    By title_article = By.xpath("//span[@class='gray-card__heading h5']");
 
     public SearchPage(WebDriver driver){
         this.driver = driver;
@@ -25,6 +25,10 @@ public class SearchPage {
         driver.findElement(search_field).isEnabled();
         driver.findElement(search_field).sendKeys(keyword);
         driver.findElement(search_field).sendKeys(Keys.ENTER);
-        FormData.setKeyword(keyword);
+    }
+
+    public List<WebElement> getTitleArticle(){
+        List<WebElement> searchResult = driver.findElements(By.xpath("//span[@class='gray-card__heading h5']"));
+        return searchResult;
     }
 }
